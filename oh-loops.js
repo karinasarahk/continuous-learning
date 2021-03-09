c37b347f9d12d5785221a435c3297497ea52e87f
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-09 21:35:34 +0700

    upload oh-loops.js file

---
+++ oh-loops.js
@@ -0,0 +1,18 @@
+// function to determine if input is even or odd
+
+function isEvenOrOdd(num) {
+    if (num % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+    // with tenary
+
+function isEvenOrOddTenary(num) {
+    return num % 2 == 0 ? "Even" : "Odd";
+}
+
+
+arr = [1, 2, 3]
