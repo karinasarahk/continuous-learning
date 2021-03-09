de95196efd6d961bed46fd83a95448486d78754f
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-09 21:38:30 +0700

    add loop over array section

--- oh-loops.js
+++ oh-loops.js
@@ -15,4 +15,19 @@ function isEvenOrOddTenary(num) {
 }
 
 
+// Loop Over Array
+
+let arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+for (;et i = 0; i < arr.length; i++) {
+    if (arr[i] > 5){
+        console.log(arr[i] * 2, "index", i);
+    }
+}
+
+
+
+
+
 arr = [1, 2, 3]
+
+
