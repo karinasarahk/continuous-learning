26ccf7e0357658031c4fa5130cfe9969be2d86b4
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-23 22:53:08 +0700

    add review w1

---
+++ Week 1/Review-W1.js
@@ -0,0 +1,9 @@
+console.log("hello this is the first line"); // commenting
+
+console.log("let's implement second push");
+
+// next let's add some branches
+
+// add branch B
+
+console.log("adding feature B");
